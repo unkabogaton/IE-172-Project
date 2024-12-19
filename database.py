4f961908679945c974fb8312fc3fcb76ae8ee4ea
mhniiixx <90919321+mhniiixx@users.noreply.github.com>
2024-12-19 11:13:14 +0800

    Add files via upload

---
+++ database.py
@@ -0,0 +1,46 @@
+import psycopg2
+from psycopg2.extras import RealDictCursor
+
+# Database connection parameters
+db_config = {
+    'dbname': 'IE172DATA',
+    'user': 'postgres',
+    'password': 'ohmcutie03',
+    'host': 'localhost',
+    'port': '5432'
+}
+
+def fetch_data(table_name, columns="*", conditions=None, order_column=None, order_direction='ASC' , limit=None):
+    try:
+        # Connect to the database
+        conn = psycopg2.connect(**db_config)
+        cursor = conn.cursor(cursor_factory=RealDictCursor)
+
+        # Create the query dynamically
+        query = f"SELECT {columns} FROM {table_name}"
+        
+        # Add conditions to the query if provided
+        if conditions:
+            query += f" WHERE {conditions}"
+        
+        # Add the order by clause
+        if order_column:
+            query += f" ORDER BY {order_column} {order_direction}"
+        
+        # Add limit if provided
+        if limit:
+            query += f" LIMIT {limit}"
+
+        # Execute the query
+        cursor.execute(query)
+        results = cursor.fetchall()
+
+        # Return results as a list of dictionaries
+        return results
+    except Exception as e:
+        print(f"Database error: {e}")
+        return []
+    finally:
+        if 'conn' in locals():
+            cursor.close()
+            conn.close()
